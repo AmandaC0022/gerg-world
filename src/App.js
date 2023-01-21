304aec9faefe2655e748df9bb889afa592af6b8e
Amanda Morgan <AmandaC0022@gmail.com>
2023-01-21 16:08:19 -0600

    created homepage welcome message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HomepageBanner from './components/Homepage-Banner';
 import Navbar from "./components/Navbar"; 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <HomepageBanner/>
     </div>
   );
 }
