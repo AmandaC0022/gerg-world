b5e6fffb7120a6504ec19d129e013b1a80b62b4e
Amanda Morgan <AmandaC0022@gmail.com>
2023-02-11 20:34:24 -0600

    added Home link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import logo from "../images/GWLogoSm.png"; 
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return ( 
@@ -8,11 +9,12 @@ const Footer = () => {
                     <img src={logo} height="70" width="70" style={{ marginRight:'10px'}} alt="Gerg's World Logo"/>  
                 </div>
                 <div className="links">
-                    <a className="hover-underline-animation"href="/">"GERG"CHRONICLES</a>
-                    <a className="hover-underline-animation"href="/">"GERG"ATIZERS</a>
-                    <a className="hover-underline-animation"href="/">"GERG"ISMS</a>
-                    <a className="hover-underline-animation"href="/">FACES OF "GERG"</a>
-                    <a className="hover-underline-animation"href="/">SHOP A "GERG"</a>
+                    <Link className="hover-underline-animation" to="/">HOME</Link>
+                    <Link className="hover-underline-animation" to="/chronicles">"GERG"CHRONICLES</Link>
+                    <Link className="hover-underline-animation" to="/">"GERG"ATIZERS</Link>
+                    <Link className="hover-underline-animation" to="/">"GERG"ISMS</Link>
+                    <Link className="hover-underline-animation" to="/">FACES OF "GERG"</Link>
+                    <Link className="hover-underline-animation" to="/">SHOP A "GERG"</Link>
                 </div>
                 <p>Copyright @2023 Gerg's World</p>
             </div>
