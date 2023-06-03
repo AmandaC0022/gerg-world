19905517a4d7292c26901478cf09e055502ef122
Amanda Morgan <AmandaC0022@gmail.com>
2023-06-03 09:15:48 -0500

    shows latest Gergal on homepage

--- client/src/components/GergalOfTheDay.js
+++ client/src/components/GergalOfTheDay.js
@@ -1,14 +1,30 @@
 import cloud from "../images/cloud.png"; 
+import { useQuery } from '@apollo/client';
+import { FIND_GERGALS } from '../utils/queries'; 
 
 const GergaloftheDay = () => {
+    //Query all of the gergals
+    const { loading, data, error } = useQuery(FIND_GERGALS); 
+    //Grabs the index from the latest gergal
+    const index = data.findGergals.length - 1; 
+    const gergal = data.findGergals; 
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    if (error) {
+        return <div>{error.message}</div>
+    }
+    
     return ( 
     <div className="GOTDcontainer">
         <h2>"Gerg"al of the Day</h2>
         <div className="cloud-container">
             <img className="cloud" src={cloud} alt="thought cloud"/> 
-            <h3 className="cloud-overlay">"Gerg"itude</h3>
+            <h3 className="cloud-overlay">{gergal[index].word}</h3>
         </div>
-        <p>Having a general positive attitude, unless you are heading to your mother's house, then it is pure rage.</p>
+        <p>{gergal[index].definition}</p>
     </div>
      );
 }
