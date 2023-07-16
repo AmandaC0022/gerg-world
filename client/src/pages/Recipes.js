df7675e72ec3448dd0a3e3d5b044585ad935d964
Amanda Morgan <AmandaC0022@gmail.com>
2023-07-15 20:25:52 -0500

    displaying all recipes in the database

--- client/src/pages/Recipes.js
+++ client/src/pages/Recipes.js
@@ -7,17 +7,17 @@ import { useQuery } from '@apollo/client';
 import { FIND_RECIPES } from '../utils/queries';
 
 const Recipes = () => {
-    // const { loading, data, error } = useQuery(FIND_RECIPES);
-    // const recipes = data?.findRecipes || []; 
+    const { loading, data, error } = useQuery(FIND_RECIPES);
+    const recipes = data?.findRecipes || []; 
     // console.log(recipes); 
 
-    // if (loading) {
-    //     return <div>Loading...</div>
-    // }
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
-    // if (error) {
-    //     return <div>{error.message}</div>
-    // }
+    if (error) {
+        return <div>{error.message}</div>
+    }
 
     return ( 
         <div>
@@ -28,36 +28,47 @@ const Recipes = () => {
                     <p>Chef Gerg is always making up some delicious meals for his kids when his wife leaves on trips. Below are some of his favorite recipes. Dads everywhere feel free to cook these when your beloved wife leaves. These are sure to liven up any occasion. </p>
                 </div>
             </div>
-            <Accordion className="recipe-container" defaultActiveKey="1" key="1">
-                <Accordion.Item eventKey="0" className="recipe-item">
-                    <div className="edit-icon">
-                        <Link to="/">
-                            <span className="material-symbols-outlined">
-                                edit
-                            </span>
-                        </Link>
-                    </div>
-                    <Accordion.Header>
-                        <div className="recipe-header">
-                            <h2>Fried Spam</h2>
-                            <img className="recipe-img" src={recipeImg}></img>
+            {recipes.map((recipe) => {
+                return (
+                <Accordion className="recipe-container" defaultActiveKey="1" key={recipe.id}>
+                    <Accordion.Item eventKey="0" className="recipe-item">
+                        <div className="edit-icon">
+                            <Link to="/">
+                                <span className="material-symbols-outlined">
+                                    edit
+                                </span>
+                            </Link>
                         </div>
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        <p><span>Category: </span>Breakfast</p>
-                        <p><span>Serves: </span>4</p>
-                        <p><span>Cook Time: </span>15 mins</p>
-                        <p><span>Ingredients: </span></p>
-                        <ul>
-                            <li>Spam</li>
-                        </ul>
-                        <p><span>Cooking Steps: </span></p>
-                        <ol>
-                            <li>Cook spam in a pan over the stove top until browned.</li>
-                        </ol>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+                        <Accordion.Header>
+                            <div className="recipe-header">
+                                <h2>{recipe.title}</h2>
+                                <img className="recipe-img" src={recipeImg}></img>
+                            </div>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <p><span>Category: </span>{recipe.category}</p>
+                            <p><span>Serves: </span>{recipe.serves}</p>
+                            <p><span>Cook Time: </span>{recipe.cooktime}</p>
+                            <p><span>Ingredients: </span></p>
+                                <ul>
+                                    {recipe.ingredients.map((ingredient) => {
+                                        return (
+                                            <li>{ingredient}</li>
+                                        )
+                                    })}
+                                </ul>
+                            <p><span>Cooking Steps: </span></p>
+                            <ol>
+                                {recipe.cookingsteps.map((step) => {
+                                    return (
+                                        <li>{step}</li>
+                                    )
+                                })}
+                            </ol>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            )})}
         </div>
      );
 }
